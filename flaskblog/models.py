82a4cbf4064d1aa12f418d0b9eddff71461074db
Sungbin Kim <sungbinkim98@gmail.com>
2020-04-05 15:44:44 -0400

    added pages/entries/goals

--- flaskblog/models.py
+++ flaskblog/models.py
@@ -10,13 +10,14 @@ def load_user(user_id):
 
 friendship = db.Table(
     'friendships', Base.metadata,
-    db. Column('friend_a_id', db.Integer, db.ForeignKey('user.id'), 
+    db. Column('friend_a_id', db.Integer, db.ForeignKey('User.id'),
                                         primary_key=True),
-    db.Column('friend_b_id', db.Integer, db.ForeignKey('user.id'), 
+    db.Column('friend_b_id', db.Integer, db.ForeignKey('User.id'),
                                             primary_key=True)
 )
 
 class User(Base, db.Model, UserMixin):
+    __tablename__ = 'User'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
@@ -26,11 +27,27 @@ class User(Base, db.Model, UserMixin):
                            primaryjoin=id==friendship.c.friend_a_id,
                            secondaryjoin=id==friendship.c.friend_b_id,
     )
-    sections = db.relationship("Section", backref="user")
+    pages = db.relationship("Page", backref="User")
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
 
-class Section(db.Model):
+class Page(Base, db.Model):
+    __tablename__ = 'Page'
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('User.id'), nullable=False)
+    title = db.Column(db.String(100), nullable=False)
+    entries = db.relationship("Entry", backref="Page")
+    goals = db.relationship("Goal", backref="Page")
+
+class Entry(Base, db.Model):
+    __tablename__ = 'Entry'
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(500), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('Page.id'), nullable=False)
+
+class Goal(Base, db.Model):
+    __tablename__ = 'Goal'
+    id = db.Column(db.Integer, primary_key=True)
+    list = db.Column(db.String(500), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('Page.id'), nullable=False)
